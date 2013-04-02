88c0bbb38af4d7917db01b52ee4b6763d1a90767
Matt Behrens <askedrelic@gmail.com>
2013-04-01 22:27:53 -0700

    add config parser + better parsing of the citibank response

--- citibank/run.py
+++ citibank/run.py
@@ -1,9 +1,16 @@
 import mechanize
 import cookielib
 import json
+import ConfigParser
+import os
+import re
 
-username = ''
-password = ''
+cfg = ConfigParser.ConfigParser()
+cfg.read(os.path.normpath(os.path.abspath('.') + '/config.ini'))
+
+username = cfg.get('citibank.com', 'username')
+password = cfg.get('citibank.com', 'password')
+
 u1 = 'https://online.citibank.com/US/JPS/portal/Home.do'
 
 br = mechanize.Browser()
@@ -49,4 +56,22 @@ br.submit()
 r2 = br.open('https://online.citibank.com/US/REST/accountsPanel/getCustomerAccounts.jws?ttc=742', data={})
 d = json.loads(r2.read())
 
-print d
+kill_tags = re.compile(r'<.*?>')
+kill_double_space = re.compile(r'\s+')
+def format_label(label):
+    label = kill_tags.sub('',label)
+    label = label.replace(':','')
+    return label.replace(' ', '_').lower()
+
+def format_value(value):
+    value = value.replace('$','').replace(' ', '').replace(',','')
+    return float(value)
+
+cardData = {format_label(x['label']) : format_value(x['value']) for x in d['accountsSummaryViewObj']['categoryList'][0]['products'][0]['accountBalViewObjList']}
+
+accountName = d['accountsSummaryViewObj']['categoryList'][0]['products'][0]['accountName']
+accountName = kill_tags.sub(' ', accountName)
+accountName = kill_double_space.sub(' ', accountName).replace('Citi &reg;', '').strip()
+
+print accountName
+print cardData
